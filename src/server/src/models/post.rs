9f27555234a92e2e8e3533b98b627ae0b4bc2514
Henrique Beckmann <henriquebeckmann2@gmail.com>
2023-10-09 00:01:30 -0300

    refactor plantuml

--- src/server/src/models/post.rs
+++ src/server/src/models/post.rs
@@ -1,5 +1,6 @@
 use diesel::{AsChangeset, Insertable, Queryable};
 use serde::{Deserialize, Serialize};
+use actix_multipart::form::{MultipartForm, tempfile::TempFile, text::Text};
 
 #[derive(Serialize, Deserialize, Debug, Clone, Queryable, Insertable, AsChangeset)]
 #[diesel(table_name = crate::repository::schema::posts)]
@@ -18,4 +19,17 @@ pub struct Post {
     pub verified: bool,
     pub published_at: chrono::NaiveDateTime,
     pub updated_at: chrono::NaiveDateTime,
+}
+
+#[derive(MultipartForm)]
+struct NewPost {
+    pub title: Text<String>,
+    pub image_url: TempFile,
+    pub date_picture: Option<Text<String>>,
+    pub description: Option<Text<String>>,
+    pub family: Option<Text<String>>,
+    pub gender: Option<Text<String>>,
+    pub specie: Option<Text<String>>,
+    pub location: Text<String>,
+    pub locality: Text<String>,
 }
